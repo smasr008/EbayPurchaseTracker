0a5aae10aa149a56b30ff312a0f750950f8ace6f
smasr008 <smasr008@fiu.edu>
2024-07-21 09:29:19 -0400

    Initial Commit

---
+++ EbayPurchaseTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EbayPurchaseTracker
+//
+//  Created by samira masri on 7/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EbayPurchaseTracker/EbayPurchaseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EbayPurchaseTrackerApp.swift
+//  EbayPurchaseTracker
+//
+//  Created by samira masri on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct EbayPurchaseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
