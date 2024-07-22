0e98b5772ecca60b69d3bb1e87fa783da67d0b12
smasr008 <smasr008@fiu.edu>
2024-07-22 17:59:16 -0400

    update

--- EbayPurchaseTracker/EbayPurchaseTrackerApp.swift
+++ EbayPurchaseTracker/EbayPurchaseTrackerApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct EbayPurchaseTrackerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView()
         }
     }
 }
--- EbayPurchaseTracker/LoginView.swift
+++ EbayPurchaseTracker/LoginView.swift
@@ -8,8 +8,71 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State private var isCreateAccountViewPresented = false
+    @State private var isPasswordCorrect: Bool = false
+    @State private var showErrorAlert: Bool = false
+    
+    
+    @State private var user: String = ""
+    @State private var password: String = ""
+    
+    
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+        NavigationStack{
+            ZStack{
+                //location 1
+                
+                //--------------------------
+                
+                VStack{
+                    //location 2
+                    
+                    //----------------------------
+                    //username field
+                    //----------------------------
+                    //location 3
+                    
+                    
+                    //----------------------------
+                    //password field
+                    //---------------------------
+                    //location 4
+                    
+                    
+                    //----------------------------
+                    //new account button
+                    //-------------------------
+                    //location 5
+                    
+                    
+                    //-------------------------
+                    //spacers
+                    //-----------------------
+                    //location 6
+                    
+                    
+                    //---------------------
+                    //login button
+                    //--------------------
+                    //location 7
+                    
+                }// end VStack
+                //------------------------
+                //location 8
+            }// end ZStack
+            
+            
+        }
+        
+        
+        
+        
+        
     }
 }
 
