ba65c1feee31a8f1e7c1c9ed9fe312609719bf94
smasr008 <smasr008@fiu.edu>
2024-07-24 09:37:20 -0400

    updated

---
+++ EbayPurchaseTracker/AppPurchaseView.swift
@@ -0,0 +1,18 @@
+//
+//  AppPurchaseView.swift
+//  EbayPurchaseTracker
+//
+//  Created by samira masri on 7/24/24.
+//
+
+import SwiftUI
+
+struct AppPurchaseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppPurchaseView()
+}
